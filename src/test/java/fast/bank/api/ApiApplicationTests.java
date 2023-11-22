7ccab23190bb978ba9781224a87fa6fd675c6705
LuisHBeck <beck.henrique.luis@gmail.com>
2023-11-22 08:10:00 -0300

    project init

---
+++ src/test/java/fast/bank/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fast.bank.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
