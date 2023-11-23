ec8eec66fc26af961a80ba85e54397a906333537
LuisHBeck <beck.henrique.luis@gmail.com>
2023-11-23 10:52:27 -0300

    feat: account listing

--- src/main/java/fast/bank/api/domain/account/dto/AccountDetailingData.java
+++ src/main/java/fast/bank/api/domain/account/dto/AccountDetailingData.java
@@ -1,4 +1,9 @@
 package fast.bank.api.domain.account.dto;
 
+import fast.bank.api.domain.account.model.Account;
+
 public record AccountDetailingData(Long user, Long number, Integer agency, Double balance, Double creditLimit) {
+    public AccountDetailingData (Account account){
+        this(account.getUser().getRegistry(), account.getNumber(), account.getAgency(), account.getBalance(), account.getCreditLimit());
+    }
 }
--- src/main/java/fast/bank/api/domain/account/repository/AccountRepository.java
+++ src/main/java/fast/bank/api/domain/account/repository/AccountRepository.java
@@ -1,7 +1,11 @@
 package fast.bank.api.domain.account.repository;
 
 import fast.bank.api.domain.account.model.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Range;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Page<Account> findAllByIsActiveTrue(Pageable pagination);
 }
