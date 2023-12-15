9fb1cd042d4ad30e666e77d4a4a30c30544d712b
LuisHBeck <beck.henrique.luis@gmail.com>
2023-12-14 22:00:34 -0300

    refactor: service class ListAccStatementService to StatementListingService

--- src/main/java/fast/bank/api/domain/statement/service/list/StatementListingService.java
+++ src/main/java/fast/bank/api/domain/statement/service/list/StatementListingService.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-public class ListAccStatementService {
+public class StatementListingService {
 
     @Autowired
     private StatementRepository statementRepository;
